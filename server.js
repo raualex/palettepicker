b9e297bde171f9503bf6a551b1df2887862c5bd1
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-27 18:19:59 -0700

    Add projects post endpoint

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const uuidv4 = require('uuid/v4');
 app.locals.projects = [];
 
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker'
 
 app.use(express.static('public'))
 
-app.get('/projects', (request, response) => {
+app.get('/api/v1/projects', (request, response) => {
   const projects = app.locals.projects
   return response.json({ projects })
 })
 
-app.get('/projects/:id', (request, response) => {
+app.get('/api/v1/projects/:id', (request, response) => {
   const project = app.locals.projects.find(proj => proj.id === id);
   
   if (project) {
     return response.status(200).json(project)
   } else {
     return response.sendStatus(404)
+  }
+})
+
+app.post('/api/v1/projects', (request, response) => {
+  const id = uuidv4()
+  const project = request.body;
+
+  if (!project) {
+    return response.status(422).send({
+      error: 'No project title provided'
+    })
+  } else {
+    app.locals.projects.push({ id, ...project });
+    return response.status(201).json({ id, ...project })
   }
 })
 
