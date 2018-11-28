8aa296f74080bd1c385bea83268b597cc22c47af
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-28 15:41:39 -0700

    Generate random color palettes and reflect changes on page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,26 @@
+$(window).on('load', generateColors)
 $('.lock-btn').on('click', toggleLock)
+$('.generate-btn').on('click', generateColors)
+
+function generateColors(event) {
+  event.preventDefault()
+  $('.color1').css('background-color', makeRandomHex())
+  $('.color2').css('background-color', makeRandomHex())
+  $('.color3').css('background-color', makeRandomHex())
+  $('.color4').css('background-color', makeRandomHex())
+  $('.color5').css('background-color', makeRandomHex())
+}
+
+function makeRandomHex() {
+  const hexSymbols = '0123456789abcdefg'
+  let hexCode = '#'
+
+  for (var i=0; i < 6; i++) {
+    hexCode += hexSymbols[Math.floor(Math.random()*16)]
+  }
+  console.log(hexCode)
+  return hexCode
+}
 
 function toggleLock(event) {
   let lockBtn = $(event.target)
