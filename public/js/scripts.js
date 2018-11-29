1b07cfd44b83e2e545d87eaa297d354db5a4e8b9
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-28 21:35:45 -0700

    Connect POST new project to FE and add color hexes for reference to DOM

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,26 @@
-$(window).on('load', generateColors)
-$('.lock-btn').on('click', toggleLock)
-$('.generate-btn').on('click', generateColors)
+$(window).on('load', generateColors);
+$('.lock-btn').on('click', toggleLock);
+$('.generate-btn').on('click', generateColors);
+$('.save-project-btn').on('click', saveProject);
 
 function generateColors(event) {
   event.preventDefault()
 
   for (var i = 1; i < 6; i++) {
     if (!$(`.${i}`).hasClass('disabled')) {
-      $(`.${i}`).css('background-color', makeRandomHex())
+      $(`.${i}`).css('background-color', makeRandomHex(`${i}`))
     }
   }
 }
 
-function makeRandomHex() {
+function makeRandomHex(num) {
   const hexSymbols = '0123456789abcdefg'
   let hexCode = '#'
 
   for (var i=0; i < 6; i++) {
     hexCode += hexSymbols[Math.floor(Math.random()*16)]
   }
-  console.log(hexCode)
+  $(`.text-${num}`).text(hexCode)
   return hexCode
 }
 
@@ -29,4 +30,19 @@ function toggleLock(event) {
 
   lockBtn.toggleClass('locked')
   lockContainer.toggleClass('disabled')
+}
+
+function saveProject(event) {
+  event.preventDefault()
+  let name = $('.project-title-input').val()
+  
+	fetch('http://localhost:3000/api/v1/projects', {
+    method: 'POST',
+    body: JSON.stringify({name}),
+    headers:{
+      'Content-Type': 'application/json'
+    }
+	})
+		.then(response => console.log(response))
+		.catch(error => console.log(error))
 }
